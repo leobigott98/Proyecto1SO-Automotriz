4f493c61bdec903b493c4a4890d16e4adc86f5d5
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2023-06-01 16:10:26 -0400

    File reader and writer classes created

---
+++ src/Files/Escritor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Files;
+
+/**
+ *
+ * @author l_a_b
+ */
+public class Escritor {
+    
+}
